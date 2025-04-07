239da02ece682d6ed7bd83462447531f8d862c7e
Chae-Yu <chaeyu2004@naver.com>
2025-04-07 18:45:52 +0900

    feat: 1단계 서블릿 구현 - 요구사항 5: 회원 목록 및 개인정보 수정 보안 강화

--- src/main/java/controller/ListUserController.java
+++ src/main/java/controller/ListUserController.java
@@ -7,6 +7,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jwp.model.User;
 
 import java.io.IOException;
@@ -14,8 +15,17 @@ import java.util.Collection;
 
 @WebServlet("/user/userList")
 public class ListUserController extends HttpServlet {
+    private static final String USER_SESSION_KEY = "user";
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Object value = session.getAttribute(USER_SESSION_KEY);
+
+        if (value == null) {
+            resp.sendRedirect("/user/login.jsp");
+            return;
+        }
+
         Collection<User> users = MemoryUserRepository.getInstance().findAll();
         req.setAttribute("users", users);
 
