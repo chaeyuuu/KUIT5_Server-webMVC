7cb3814c97f2ce6a4e90bb3c482e1de754f0a1b4
Chae-Yu <chaeyu2004@naver.com>
2025-04-07 18:01:58 +0900

    feat: 1단계 서블릿 구현 - 요구사항 3: 개인정보 수정

---
+++ src/main/java/controller/UpdateUserController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import core.db.MemoryUserRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jwp.model.User;
+
+import java.io.IOException;
+
+@WebServlet("/user/update")
+public class UpdateUserController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        String password = req.getParameter("password");
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+
+        User updateUser = new User(userId, password, name, email);
+        MemoryUserRepository.getInstance().changeUserInfo(updateUser);
+        System.out.println("수정 완료");
+
+        resp.sendRedirect("/user/userList");
+    }
+}
